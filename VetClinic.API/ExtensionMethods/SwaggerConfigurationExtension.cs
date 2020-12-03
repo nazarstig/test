c2ff9562f6338ca6b50d8ff8d9dda0bc0f3c85e6
Illia Koziarskyi <illya_kozyarskiy@ukr.net>
2020-12-03 17:32:15 +0200

    Add Swagger
    Add swagger and cofiguration to it via extension methods in
    SwaggerConfigurationExtension.cs

---
+++ VetClinic.API/ExtensionMethods/SwaggerConfigurationExtension.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
+namespace VetClinic.ExtensionMethods
+{
+    public static class SwaggerConfigurationExtension
+    {
+        public static void AddSwaggerConfig(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(options => {
+                options.SwaggerDoc("v1",
+                new OpenApiInfo { Title = "VetClinic API", Version = "v1" });
+            });
+        }
+
+        public static void UseCustomSwaggerConfig(this IApplicationBuilder app)
+        {
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(options => {
+                options.SwaggerEndpoint("/swagger/v1/swagger.json",
+                "VetClinic API");
+            });
+        }
+    }
+}
