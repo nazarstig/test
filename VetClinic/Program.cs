089de2ff71f5643db76974634ea911d759243033
Nick <yankivnick@gmail.com>
2020-12-03 15:05:15 +0200

    Rename project

--- VetClinic/Program.cs
+++ VetClinic/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace VetClinic
+namespace VetClinic.API
 {
     public class Program
     {
--- VetClinic/Startup.cs
+++ VetClinic/Startup.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace VetClinic
+namespace VetClinic.API
 {
     public class Startup
     {
