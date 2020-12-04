df2660920a7c022fea534987e0dd56536a129292
Nick <yankivnick@gmail.com>
2020-12-04 14:02:20 +0200

    Add BaseRepository

---
+++ VetClinic.DAL/Repositories/Interfaces/IRepositoryBase.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using VetClinic.DAL.Entities;
+
+namespace VetClinic.DAL.Repositories.Interfaces
+{
+    public interface IRepositoryBase<TEntity> where TEntity : IBaseEntity
+    {
+        IQueryable<TEntity> GetAsync();
+        Task<TEntity> GetAsync(int id);
+        void Add(TEntity entity);
+        void Remove(TEntity entity);
+        void Update(TEntity entity);
+    }
+}
