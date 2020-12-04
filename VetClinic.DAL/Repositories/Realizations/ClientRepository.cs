485350f46a4c12453e3a687b113b119d2094a949
Illia Koziarskyi <illya_kozyarskiy@ukr.net>
2020-12-04 15:16:17 +0200

    Add Client and Status repositories

---
+++ VetClinic.DAL/Repositories/Realizations/ClientRepository.cs
@@ -0,0 +1,10 @@
+﻿using VetClinic.DAL.Entities;
+using VetClinic.DAL.Repositories.Interfaces;
+
+namespace VetClinic.DAL.Repositories.Realizations
+{
+    public class ClientRepository : RepositoryBase<Client>, IClientRepository
+    {
+        public ClientRepository(ApplicationContext context) : base(context) { }
+    }
+}
