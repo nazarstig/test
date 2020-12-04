5f2ab39d13052c97326f9bd7f2ba210af8f77d59
Andriy <andriysusak123@gmail.com>
2020-12-04 15:55:38 +0200

    Add animal and animalType repositories

---
+++ VetClinic.DAL/Repositories/Realizations/AnimalRepository.cs
@@ -0,0 +1,11 @@
+﻿using VetClinic.DAL.Entities;
+using VetClinic.DAL.Repositories.Interfaces;
+
+
+namespace VetClinic.DAL.Repositories.Realizations
+{
+    public class AnimalRepository : RepositoryBase<Animal>, IAnimalRepository
+    {
+        public AnimalRepository(ApplicationContext context) : base(context) { }
+    }
+}
---
+++ VetClinic.DAL/Repositories/Realizations/AnimalTypeRepository.cs
@@ -0,0 +1,10 @@
+﻿using VetClinic.DAL.Entities;
+using VetClinic.DAL.Repositories.Interfaces;
+
+namespace VetClinic.DAL.Repositories.Realizations
+{
+    class AnimalTypeRepository : RepositoryBase<AnimalType> , IAnimalTypeRepository
+    {
+        public AnimalTypeRepository(ApplicationContext context) : base(context) { }
+    }
+}
