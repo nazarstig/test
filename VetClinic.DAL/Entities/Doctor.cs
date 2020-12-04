f0f2a0e28f71fae690ce5a1cf6470c4a980ebb74
Bohdan <Yosypenko>
2020-12-04 12:25:35 +0200

    Add doctor and position entity

---
+++ VetClinic.DAL/Entities/Doctor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace VetClinic.DAL.Entities
+{
+    public class Doctor : User
+    {
+        public int DoctorId { get; set; }
+        public string Education { get; set; }
+        public string Biography { get; set; }
+        public string Experience { get; set; }
+        public string Photo { get; set; }
+        public int PositionId { get; set; }
+
+        public Position Position { get; set; }
+        public ICollection<Appointment> Appointments { get; set; }
+    }  
+}
---
+++ VetClinic.DAL/Entities/Position.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace VetClinic.DAL.Entities
+{
+    public class Position : IBaseEntity
+    {
+        public int Id { get; set; }
+        public string PositionName { get; set; }
+        public decimal Salary { get; set; }
+
+        public ICollection<Doctor> Doctors { get; set; }
+    }
+}
