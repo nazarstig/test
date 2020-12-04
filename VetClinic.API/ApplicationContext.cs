f7984ed6cba7ebaa06da330d912fffa984571ab5
nazarstig <nazarstig@gmail.com>
2020-12-04 10:59:10 +0200

    Configuration of database

---
+++ VetClinic.API/ApplicationContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace VetClinic.DAL
+{
+    class ApplicationContext : DbContext
+    {
+    }
+}
