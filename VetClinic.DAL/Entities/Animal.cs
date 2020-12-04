71d299dbd1e7c7a987d69acc5090db53789bd92a
Andriy <andriysusak123@gmail.com>
2020-12-04 12:31:10 +0200

    Add animal and animalType entities

---
+++ VetClinic.DAL/Entities/Animal.cs
@@ -0,0 +1,15 @@
+﻿namespace VetClinic.DAL.Entities
+{
+    class Animal : IBaseEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Photo { get; set; }
+
+        public int ClientId { get; set; }
+        public Client Client { get; set; }
+        public int AnimalTypeId { get; set; }
+        public AnimalType AnimalType { get; set; }
+    }
+}
