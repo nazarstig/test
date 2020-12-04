8a8792f6e7beec608f105ccf11e2f910ee42519f
nazarstig <nazarstig@gmail.com>
2020-12-04 12:09:35 +0200

    Added services and procedure

---
+++ VetClinic.DAL/Entities/Procedure.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace VetClinic.DAL.Entities
+{
+    public class Procedure : IBaseEntity
+    {
+        public int Id { get; set; }
+        public string ProcedureName { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public bool IsSelectable { get; set; }
+
+        public ICollection<Appointment> Appointments { get; set; }
+    }
+}
---
+++ VetClinic.DAL/Entities/Service.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VetClinic.DAL.Entities
+{
+    class Service : IBaseEntity
+    {
+        public int Id { get; set; }
+        public string ServiceName { get; set; }
+
+        public ICollection<Appointment> Appointments { get; set; }
+    }
+}
