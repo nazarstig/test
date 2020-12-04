83595343ee4af467a526329fa9e8383b5f7b8853
Nick <yankivnick@gmail.com>
2020-12-04 16:00:12 +0200

    Add AppointmentProceduresRepository

---
+++ VetClinic.DAL/Repositories/Realizations/AppointmentProceduresRepository.cs
@@ -0,0 +1,11 @@
+﻿using VetClinic.DAL.Entities;
+using VetClinic.DAL.Repositories.Interfaces;
+
+namespace VetClinic.DAL.Repositories.Realizations
+{
+    public class AppointmentProceduresRepository : RepositoryBase<AppointmentProcedures>,
+        IAppointmentProceduresRepository
+    {
+        public AppointmentProceduresRepository(ApplicationContext context) : base(context) { }
+    }
+}
