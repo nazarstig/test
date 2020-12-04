1d4cfb5f0571331d73793aa061287d97b7462b08
Arsen <arsenyaremin@gmail.com>
2020-12-04 10:44:47 +0200

    Add User and IBaseEntity

---
+++ VetClinic.DAL/Entities/IBaseEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VetClinic.DAL.Entities
+{
+    interface IBaseEntity
+    {
+        int Id { get; set; }
+    }
+}
