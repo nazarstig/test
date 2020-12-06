287b6f05c4d70e6c70c617b63f3cf7049f7da20e
Arsen <arsenyaremin@gmail.com>
2020-12-06 22:21:33 +0200

    Removed damaged IS4 project

--- IdentityServerRoles/Config.cs
+++ IdentityServerRoles/Config.cs
@@ -1,76 +1,84 @@
-﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
-// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
-
-
+﻿using IdentityModel;
 using IdentityServer4;
 using IdentityServer4.Models;
 using System.Collections.Generic;
-using System.Security.Claims;
 
 namespace Is4RoleDemo
 {
     public static class Config
     {
         public static IEnumerable<IdentityResource> IdentityResources =>
-            new IdentityResource[]
-            {
+                   new IdentityResource[]
+                   {
                 new IdentityResources.OpenId(),
+                //new IdentityResources.Email(),
                 new IdentityResources.Profile(),
-            };
+                   };
 
         public static IEnumerable<ApiScope> ApiScopes =>
             new ApiScope[]
             {
                 new ApiScope("scope1"),
                 new ApiScope("scope2"),
+                new ApiScope("ApiOne"),
             };
 
         public static IEnumerable<Client> Clients =>
+
             new Client[]
             {
+
                 new Client
                 {
                     ClientId = "postman",
                     RequirePkce = true,
                     Enabled = true,
 
-                    AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
-                    ClientSecrets = { new Secret("511536EF-F270-4058-80CA-1C89C192F69A".Sha256()) },
+                    AllowedGrantTypes = GrantTypes.CodeAndClientCredentials,
+                    ClientSecrets = { new Secret("secret".ToSha256()) },
+
+                    RedirectUris = { "https://oauth.pstmn.io/v1/browser-callback" },
 
                     AllowedScopes = {
                         IdentityServerConstants.StandardScopes.OpenId,
                         IdentityServerConstants.StandardScopes.Profile,
-                        "scope1"
+                        "scope1",
+                        "scope2"
+                    }
+                },
+                // m2m client credentials flow client
+                new Client()
+                {
+                    ClientId = "client_id",
+                    ClientSecrets = {new Secret("client_secret".ToSha256()) },
+
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    RedirectUris = { "https://oauth.pstmn.io/v1/browser-callback" },
+
+                    AllowedScopes = {"ApiOne"},
+                    RequireConsent = false,
+
+                },
+
+                new Client()
+                {
+                    ClientId = "angular_client",
+                    RequirePkce = true,
+                    ClientSecrets = {new Secret("angular_secret".ToSha256()) },
+
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RedirectUris = { "https://oauth.pstmn.io/v1/browser-callback",
+                                       "https://localhost:44334/signin-oidc", },
+
+
+                    AllowedScopes = new List<string>
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        "ApiOne"
                     }
                 },
 
-                // m2m client credentials flow client
-                new Client
-                {
-                    ClientId = "m2m.client",
-                    ClientName = "Client Credentials Client",
-
-                    AllowedGrantTypes = GrantTypes.ClientCredentials,
-                    ClientSecrets = { new Secret("511536EF-F270-4058-80CA-1C89C192F69A".Sha256()) },
-
-                    AllowedScopes = { "scope1" }
-                },
-
-                // interactive client using code flow + pkce
-                new Client
-                {
-                    ClientId = "interactive",
-                    ClientSecrets = { new Secret("49C1A7E1-0C79-4A89-A3D6-A37998FB86B0".Sha256()) },
-
-                    AllowedGrantTypes = GrantTypes.Code,
-
-                    RedirectUris = { "https://localhost:44300/signin-oidc" },
-                    FrontChannelLogoutUri = "https://localhost:44300/signout-oidc",
-                    PostLogoutRedirectUris = { "https://localhost:44300/signout-callback-oidc" },
-
-                    AllowOfflineAccess = true,
-                    AllowedScopes = { "openid", "profile", "scope1" }
-                },
             };
     }
 }
