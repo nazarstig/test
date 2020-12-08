7b5f5ca41f4f6291341b312560c216d7c9a9dd56
BohdanYosypenko <idybp28@gmail.com>
2020-12-08 19:28:55 +0200

    Add initial seed

--- VetClinic.DAL/Configurations/StatusesConfiguration.cs
+++ VetClinic.DAL/Configurations/StatusesConfiguration.cs
@@ -11,7 +11,16 @@ namespace VetClinic.DAL.Configurations
         {
             builder.Property(t => t.StatusName)
                 .HasConversion(new EnumToStringConverter<StatusName>())
-                .HasMaxLength(20);         
+                .HasMaxLength(20);
+
+            builder.HasData(
+                new Status[]
+                {
+                    new Status{ Id=1, StatusName=StatusName.Approved},
+                    new Status{ Id=2, StatusName=StatusName.Disapproved},
+                    new Status{ Id=3, StatusName=StatusName.Processing},
+                    new Status{ Id=4, StatusName=StatusName.Completed},
+                });
         }
     }
 }
