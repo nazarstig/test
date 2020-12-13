e604814c975259404f38f0275b91275bcd5b4dbb
BohdanYosypenko <idybp28@gmail.com>
2020-12-13 22:28:51 +0200

    Add pos test

---
+++ VetClinic.API.Tests/PositionControllerTests.cs
@@ -0,0 +1,18 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace VetClinic.API.Tests
+{
+    public class PositionControllerTests
+    {
+        private readonly HttpClient httpClient = new HttpClient();
+        [Fact]           
+        public async Task PosotionControllerGetRequestExpected()
+        {
+            var responce = await httpClient.GetAsync("https://localhost:44300/api/position/1");            
+            Assert.Equal(HttpStatusCode.OK, responce.StatusCode);               
+        }
+    }
+}
