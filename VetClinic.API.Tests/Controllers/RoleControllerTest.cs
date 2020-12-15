13cc6f48884dc4d03c711ae29914d6296f8628a9
Arsen <arsenyaremin@gmail.com>
2020-12-15 12:04:42 +0200

    Add empty role controller

---
+++ VetClinic.API.Tests/Controllers/RoleControllerTest.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentAssertions;
+using System.Text.Json;
+using System.Threading.Tasks;
+using VetClinic.API.Controllers;
+using Xunit;
+
+namespace VetClinic.API.Tests.Controllers
+{
+    public class RoleControllerTest 
+    {
+        [Fact]
+        public async Task Get_NoParams_ReturnAllRolesInJson()
+        {
+            //arrange
+            var roles = new List<IdentityRole>()
+            {
+                new IdentityRole()
+                {
+                    Name = "pucci"
+                },
+                new IdentityRole()
+                {
+                    Name = "gucci"
+                }
+            };
+            //var rolesJson = JsonSerializer.Serialize(roles);
+                
+            var roleStore = new Mock<IRoleStore<IdentityRole>>();
+            var roleManagerMock = new Mock<RoleManager<IdentityRole>>(roleStore.Object, null, null, null, null);
+
+            var controller = new RoleController(roleManagerMock.Object);
+
+
+            //act
+            var result = await controller.Get();
+            var contentResult = result as ObjectResult;
+            var content = JsonSerializer.Deserialize<List<IdentityRole>>(contentResult.Value.ToString());
+
+            //assert
+            Assert.NotNull(result);
+            Assert.IsType<OkObjectResult>(result);
+           // Assert.Equal(roles, contentResult.Value);
+            content.Should().Contain(content => content.Name == "admn");
+        }
+    }
+}
---
+++ VetClinic.API/Controllers/RoleController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VetClinic.API.Controllers
+{
+    public class RoleController : Controller
+    {
+        public RoleController(RoleManager<IdentityRole> roleManager)
+        {
+            RoleManager = roleManager;
+        }
+
+        public RoleManager<IdentityRole> RoleManager { get; }
+
+        public async Task<IActionResult> Get()
+        {
+            return Ok(new { name ="arrgh"});
+        }
+    }
+}
