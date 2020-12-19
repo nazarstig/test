833c1f1b6af0c42a38f7d4e3aa84cd7a76c39de3
Nick <yankivnick@gmail.com>
2020-12-19 14:00:25 +0200

    Add ValidationFilter and ExceptionMiddleware

--- VetClinic.API/Startup.cs
+++ VetClinic.API/Startup.cs
@@ -1,18 +1,22 @@
+using AutoMapper;
+using FluentValidation.AspNetCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
-using AutoMapper;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using VetClinic.API.ExtensionMethods;
-using VetClinic.DAL;
-using VetClinic.DAL.Repositories.Interfaces;
-using VetClinic.DAL.Repositories.Realizations;
-using VetClinic.DAL.Entities;
-using Microsoft.AspNetCore.Identity;
+using VetClinic.API.Filters;
+using VetClinic.API.Middlewares;
 using VetClinic.BLL.Services.Interfaces;
 using VetClinic.BLL.Services.Realizations;
+using VetClinic.DAL;
+using VetClinic.DAL.Entities;
+using VetClinic.DAL.Repositories.Interfaces;
+using VetClinic.DAL.Repositories.Realizations;
 
 namespace VetClinic.API
 {
@@ -28,18 +32,18 @@ namespace VetClinic.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddAuthentication("RefAndJWTToken")
-            .AddIdentityServerAuthentication("RefAndJWTToken", options =>
-            {
-                options.Authority = "https://localhost:5005";
-                options.ApiName = "VetClinicApi";
-                options.ApiSecret = "angular_secret";
-            });
+                .AddIdentityServerAuthentication("RefAndJWTToken", options =>
+                {
+                    options.Authority = "https://localhost:5005";
+                    options.ApiName = "VetClinicApi";
+                    options.ApiSecret = "angular_secret";
+                });
 
             string connection = Configuration.GetConnectionString("DefaultConnection");
             services.AddDbContext<ApplicationContext>(options =>
                 options.UseSqlServer(connection, builder =>
                     builder.MigrationsAssembly("VetClinic.DAL")));
-           
+
 
             IdentityBuilder builder = services.AddIdentityCore<User>();
             builder = new IdentityBuilder(typeof(User), typeof(IdentityRole), builder.Services);
@@ -52,7 +56,11 @@ namespace VetClinic.API
 
             services.AddAutoMapper(typeof(Startup));
 
-            services.AddControllers();
+            services.AddControllers(options => { options.Filters.Add(new ValidationFilter()); })
+                .AddFluentValidation(options => { options.RegisterValidatorsFromAssemblyContaining<Startup>(); })
+                .AddNewtonsoftJson();
+
+            services.Configure<ApiBehaviorOptions>(options => { options.SuppressModelStateInvalidFilter = true; });
 
             services.AddScoped<IRepositoryWrapper, RepositoryWrapper>();
             services.AddScoped<IUserService, UserService>();
@@ -68,16 +76,15 @@ namespace VetClinic.API
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseMiddleware<ExceptionMiddleware>();
+
             app.UseRouting();
 
             app.UseAuthentication();
 
             app.UseAuthorization();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
+            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
 
             app.UseCustomSwaggerConfig();
 
