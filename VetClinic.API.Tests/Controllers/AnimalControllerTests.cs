8c88391c3f59775273eebd2c056fd1a36bedc5ef
Andriy <andriysusak123@gmail.com>
2020-12-23 15:07:56 +0200

    Fix some tests

--- VetClinic.API.Tests/Controllers/AnimalControllerTests.cs
+++ VetClinic.API.Tests/Controllers/AnimalControllerTests.cs
@@ -2,12 +2,9 @@
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
-using System;
-using System.Collections.Generic;
 using System.Threading.Tasks;
 using VetClinic.API.Controllers;
 using VetClinic.API.DTO;
-using VetClinic.API.DTO.Animal;
 using VetClinic.API.Mapping;
 using VetClinic.BLL.Services.Interfaces;
 using VetClinic.DAL.Entities;
@@ -17,19 +14,19 @@ namespace VetClinic.API.Tests.Controllers
 {
     public class AnimalControllerTests
     {
-        readonly Mock<IAnimalService> _animalService;
         IMapper _mapper;
 
         public AnimalControllerTests()
         {
-            _animalService = new Mock<IAnimalService>();
             var profile = new ReadAnimalProfile();
+            var profile1 = new CreateAnimalProfile();
             var configuration = new MapperConfiguration(cfg => cfg.AddProfile(profile));
             _mapper = new Mapper(configuration);
         }
 
-        [Fact]
-        public async Task GetAsync_WhenCalled_ReturnsOkObjectResult()
+        [Theory,AutoMoqData]
+        public async Task GetAsync_WhenCalled_ReturnsOkObjectResult(
+            [Frozen] Mock<IAnimalService> _animalService)
         {
             //Arrange
             var Sut = new AnimalsController(_animalService.Object, _mapper);
@@ -42,8 +39,9 @@ namespace VetClinic.API.Tests.Controllers
             Assert.IsType<OkObjectResult>(actual);
         }
 
-        [Fact]
-        public async Task GetAsync_AnimalId_ReturnsOkObjectResult()
+        [Theory, AutoMoqData]
+        public async Task GetAsync_AnimalId_ReturnsOkObjectResult(
+            [Frozen] Mock<IAnimalService> _animalService)
         {
             //Arrange
             int id = 2;
@@ -57,8 +55,9 @@ namespace VetClinic.API.Tests.Controllers
             Assert.IsType<OkObjectResult>(actual);
         }
 
-        [Fact]
-        public async Task PostAsync_CreateAnimalDto_ReturnsCreateAnimalDto()
+        [Theory, AutoMoqData]
+        public async Task PostAsync_CreateAnimalDto_ReturnsCreateAnimalDto(
+            [Frozen] Mock<IAnimalService> _animalService)
         {
             //Arrange
             var animalDto = new Mock<CreateAnimalDto>();
@@ -68,8 +67,44 @@ namespace VetClinic.API.Tests.Controllers
             var actual = await Sut.PostAsync(animalDto.Object);
 
             //Assert
+            _animalService.Verify(x => x.CreateAnimal(It.IsAny<Animal>()));
             Assert.NotNull(actual);
             Assert.IsType<OkObjectResult>(actual);
+        }
+
+        [Theory, AutoMoqData]
+        public async Task PutAsync_UpdateAnimalDto_PutSuccessReturnsNoContent(
+            [Frozen] Mock<IAnimalService> _animalService)
+        {
+            //Arrange
+            int id = 1;
+            var animalDto = new Mock<UpdateAnimalDto>();
+            var Sut = new AnimalsController(_animalService.Object, _mapper);
+
+            //Act
+            var actual = await Sut.PutAsync(id, animalDto.Object);
+
+            //Assert
+            _animalService.Verify(x => x.UpdateAnimal(It.IsAny<Animal>()));
+            Assert.NotNull(actual);
+            Assert.IsType<NoContentResult>(actual);
+        }
+
+        [Theory, AutoMoqData]
+        public async Task DeleteAsync_AnimalId_DeleteSuccessReturnsNoContent(
+            [Frozen] Mock<IAnimalService> _animalService)
+        {
+            //Arrange
+            int id = 1;
+            var Sut = new AnimalsController(_animalService.Object, _mapper);
+
+            //Act
+            var actual = await Sut.DeleteAsync(id);
+
+            //Assert
+            _animalService.Verify(x => x.RemoveAnimal(It.IsAny<Animal>()));
+            Assert.NotNull(actual);
+            Assert.IsType<NoContentResult>(actual);
         }
     }
 }
---
+++ VetClinic.API/Mapping/CreateAnimalProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using VetClinic.API.DTO;
+using VetClinic.DAL.Entities;
+
+namespace VetClinic.API.Mapping
+{
+    public class CreateAnimalProfile : Profile
+    {
+        public CreateAnimalProfile()
+        {
+            CreateMap<CreateAnimalDto, Animal>()
+                .ForMember(d => d.Name, t => t.MapFrom(o => o.Name))
+                .ForMember(d => d.Age, t => t.MapFrom(o => o.Age))
+                .ForMember(d => d.Photo, t => t.MapFrom(o => o.Photo))
+                .ForMember(d => d.ClientId, t => t.MapFrom(o => o.ClientId))
+                .ForMember(d => d.AnimalTypeId, t => t.MapFrom(o => o.AnimalTypeId))
+                .ReverseMap();
+        }
+    }
+}
