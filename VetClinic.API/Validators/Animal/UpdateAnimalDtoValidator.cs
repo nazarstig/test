9a874661c50faa8cb3af4045894a3bd9bd36403f
Andriy <andriysusak123@gmail.com>
2021-01-15 12:32:59 +0200

    Add validators to animal

---
+++ VetClinic.API/Validators/Animal/UpdateAnimalDtoValidator.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+using VetClinic.API.DTO.Animal;
+
+namespace VetClinic.API.Validators.Animal
+{
+    public class UpdateAnimalDtoValidator:AbstractValidator<UpdateAnimalDto>
+    {
+        public UpdateAnimalDtoValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .WithMessage("Should not be empty")
+                .MaximumLength(20)
+                .WithMessage("Length should be less than 21");
+
+            RuleFor(x => x.Age)
+                .GreaterThan(-1)
+                .WithMessage("Age can not be negative");
+
+            RuleFor(x => x.Photo)
+                .MaximumLength(150)
+                .WithMessage("Length should be less than 151");
+
+            RuleFor(x => x.AnimalTypeId)
+                .NotEmpty()
+                .WithMessage("Should not be empty");
+        }
+    }
+}
