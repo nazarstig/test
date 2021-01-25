480e307f6655ec8761e75f652a8893e87742bdda
Arsen <arsenyaremin@gmail.com>
2021-01-25 22:17:57 +0200

    Add account controller
    Add user exists check

---
+++ VetClinic.API/Controllers/AccountController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VetClinic.BLL.Services.Interfaces;
+
+namespace VetClinic.API.Controllers
+{
+    [Route("api/account")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        public AccountController(IUserService userService)
+        {
+            UserService = userService;
+        }
+
+        public IUserService UserService { get; }
+
+        [HttpGet("{userName}")]
+        public async Task<bool> UserNameExists(string userName)
+        {
+            return await UserService.UserNameExistsAsync(userName);
+        }
+    }
+}
