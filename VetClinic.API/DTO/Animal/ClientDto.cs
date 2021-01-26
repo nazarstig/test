8d35a115e3ed460d2a28846cdb2490849fad3f35
Andriy <andriysusak123@gmail.com>
2021-01-26 20:08:30 +0200

    Change ReadAnimalDto and response in AnimalTypeController

---
+++ VetClinic.API/DTO/Animal/ClientDto.cs
@@ -0,0 +1,9 @@
+﻿namespace VetClinic.API.DTO.Animal
+{
+    public class ClientDto
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
